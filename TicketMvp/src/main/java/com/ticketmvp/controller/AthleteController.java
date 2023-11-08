b2db9a38334c5bc83f68dcb4dc747ae4dfd0b9ab
ICT03-05 <ICT03-05@114.207.167.102>
2023-11-08 17:54:45 +0900

    11/08 김유빈

--- TicketMvp/src/main/java/com/ticketmvp/controller/AthleteController.java
+++ TicketMvp/src/main/java/com/ticketmvp/controller/AthleteController.java
@@ -8,8 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.servlet.http.HttpSession;
 
 import com.ticketmvp.domain.AthleteVO;
+import com.ticketmvp.domain.MatchVO;
 import com.ticketmvp.service.AthleteService;
 
 @Controller
@@ -19,21 +21,31 @@ public class AthleteController {
 	@Autowired
 	private AthleteService athleteService;
 
-	  @RequestMapping("main_page.do")
-	    public String showMain(Model model) {
-	        List<AthleteVO> athletes = athleteService.getAthleteImagePaths();
-	        model.addAttribute("athletes", athletes);
-	        return "main_page";
-	    }
-
-// athlete_information.do에 해당하는 메소드
-
-
-	  @RequestMapping("athlete_information.do")
-	  public String showAthleteInformation(@RequestParam("athletename") String athleteName, Model model) {
-	      AthleteVO athlete = athleteService.getAthleteInformation(athleteName);
-	      model.addAttribute("athlete", athlete);
-	      return "athlete/athlete_information";
-	  }
+// 메인페이지에서 선수 정보 띄우기
+	@RequestMapping("main_page.do")
+	public String showMain(Model model) {
+		List<AthleteVO> athletes = athleteService.getAthleteImagePaths();
+		model.addAttribute("athletes", athletes);
+		return "main_page";
 	}
 
+// athlete_information 페이지에서 선수 이미지, 정보, 경기 일정 띄우기
+	@RequestMapping("athlete_information.do")
+	public String showAthleteInformation(@RequestParam("athletename") String athleteName, Model model) {
+		
+		// 선수 이미지, 정보 띄우기.
+		AthleteVO athlete = athleteService.getAthleteInformation(athleteName);
+		model.addAttribute("athlete", athlete);
+		
+		// 선수가 속한 구단의 경기 일정 가져오기
+		List<MatchVO> matches =  athleteService.getMatchesForClub(athleteName);
+		
+		 model.addAttribute("matches", matches);
+		 
+		 System.out.println("Matches: " + matches);
+		 
+		return "athlete/athlete_information";
+	}
+	
+
+}
--- TicketMvp/src/main/java/com/ticketmvp/dao/AthleteDAOImpl.java
+++ TicketMvp/src/main/java/com/ticketmvp/dao/AthleteDAOImpl.java
@@ -6,20 +6,39 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.ticketmvp.domain.AthleteVO;
+import com.ticketmvp.domain.MatchVO;
 
 @Repository("athleteDAO")
 public class AthleteDAOImpl implements AthleteDAO {
 
-    @Autowired
-    private SqlSessionTemplate mybatis;
+	@Autowired
+	private SqlSessionTemplate mybatis;
 
-    @Override
-    public List<AthleteVO> getAthleteImagePaths() {
-        return mybatis.selectList("com.ticketmvp.dao.AthleteDAO.getAthleteImagePaths");
-    }
+	@Override
+	public List<AthleteVO> getAthleteImagePaths() {
+		return mybatis.selectList("com.ticketmvp.dao.AthleteDAO.getAthleteImagePaths");
+	}
 
-    @Override
-    public AthleteVO getAthleteInformation(String athleteName) {
-        return mybatis.selectOne("com.ticketmvp.dao.AthleteDAO.getAthleteInformation", athleteName);
-    }
+	@Override
+	public AthleteVO getAthleteInformation(String athleteName) {
+		return mybatis.selectOne("com.ticketmvp.dao.AthleteDAO.getAthleteInformation", athleteName);
+	}// 선수 이미지, 선수 정보
+
+	@Override
+	public List<MatchVO> getMatchesForClub(String athletename) {
+		System.out.println("파라미터 선수이름: "+athletename);
+		
+		String club = mybatis.selectOne("com.ticketmvp.dao.AthleteDAO.getMatchesAthleteClub", athletename);
+		System.out.println("클럽명 : "+club);
+		
+		List<MatchVO> result = mybatis.selectList("com.ticketmvp.dao.AthleteDAO.getMatchesForClub", club);
+		System.out.println("경기일정 "+result.toString());
+		return result;
+	}
+
+	/*
+	 * @Override public List<MatchVO> getAthleteMatches(String club) { return
+	 * mybatis.selectList("com.ticketmvp.dao.AthleteDAO.getAthleteMatches", club); }
+	 */
+
 }
